3106e99279742b74b14693653583f5b3f414afdb
DominikMoore <dominik1049358@my.yosemite.edu>
2025-08-31 15:15:15 -0700

    explained each step

--- main.cpp
+++ main.cpp
@@ -1,10 +1,11 @@
 //Dominik Moore
 //1 September 2025
 //MJC Comp Sci 2
+//The purpose of the program is to calculate the area and or perimeter of a circle or rectangle
 
 #include <iostream>
 #include <limits>
-using namespace std;
+using namespace std;                                        //dont have to keep writing std::cout/cin
 
 double calculateArea (const double radius) {                // calculate area of circle
     const double PI = 3.14159;
@@ -28,28 +29,28 @@ int main() {
     int usrInput;
     double radius, length, width;
     do {
-        cout << "Welcome to Geometric Calculations!\n**********************************" << endl;
-        cout << "Please Pick What to Calculate." << endl;
-        cout << "Please Enter the Number 1 to Calculate the Area of a Circle." << endl;
-        cout << "Please Enter the Number 2 to Calculate the Perimeter of a Circle." << endl;
-        cout << "Please Enter the Number 3 to Calculate the Area of a Rectangle." << endl;
-        cout << "Please Enter the Number 4 to Calculate the Perimeter of a Rectangle." << endl;
-        cout << "Please Enter the Number 5 to Exit the Program." << endl;
+        cout << "Welcome to Geometric Calculations!\n**********************************" << endl;           //
+        cout << "Please Pick What to Calculate." << endl;                                                   //
+        cout << "Please Enter the Number 1 to Calculate the Area of a Circle." << endl;                     //
+        cout << "Please Enter the Number 2 to Calculate the Perimeter of a Circle." << endl;                //"menu"
+        cout << "Please Enter the Number 3 to Calculate the Area of a Rectangle." << endl;                  //
+        cout << "Please Enter the Number 4 to Calculate the Perimeter of a Rectangle." << endl;             //
+        cout << "Please Enter the Number 5 to Exit the Program." << endl;                                   //
 
         cin >> usrInput;
-        if (cin.fail()) {
-            cout << "Invalid Character's, Please Enter a Number." << endl;
+        if (cin.fail()) {                                                                           //validates code
+            cout << "Invalid Character's, Please Enter a Number." << endl;                          //makes sure it is good input
             cin.clear();
             cin.ignore(numeric_limits<streamsize>::max(),'\n');
             continue;
         }
-        if (usrInput < 1 || usrInput > 5 ) {
+        if (usrInput < 1 || usrInput > 5 ) {                                                        //makes sure only a number 1-5
             cout << "Invalid Number Please Try again." << endl;
             continue;
         }
 
-        switch (usrInput) {
-            case 1: // Area of circle
+        switch (usrInput) {                                                                         //switch to allow the user to pick
+            case 1: // Area of circle                                                               // and enter data
                 cout << "Enter the radius: ";
                 cin >> radius;
                 cout << "Area of a Circle: " << calculateArea(radius) << endl;
@@ -80,8 +81,8 @@ int main() {
                 cout << "Exiting Program. Thank you!" << endl;
                 break;
         }
-    }   while (usrInput != 5);
-
+    }   while (usrInput != 5);                                                                  // if input is not 5, the code will continue
+                                                                                                //once input is 5 the code will come to an end
     return 0;
 }
 
